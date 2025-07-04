841bcb56d2f84eaff4b7a41e6840d3526215a200
Tjaos <th83287716@gmail.com>
2025-07-04 09:01:45 -0300

    feat: criação do projeto e estruturação inicial das classes Money e MoneyAudit e AccountWallet, juntamente com o Enum BankService.

---
+++ src/main/java/br/com/dio/model/AccountWallet.java
@@ -0,0 +1,20 @@
+package br.com.dio.model;
+
+import java.util.List;
+import static br.com.dio.model.BankService.ACCOUNT;
+
+public class AccountWallet extends Wallet{
+
+    private final List<String> pix;
+
+    public AccountWallet(final List<String> pix) {
+        super(ACCOUNT);
+        this.pix = pix;
+    }
+
+    public AccountWallet(final long amount, List<String> pix) {
+        super(ACCOUNT);
+        this.pix = pix;
+        addMoney(generateMoney(amount, "Valor de criação da conta"), ACCOUNT, "Deposito");
+    }
+}
